1823d273c0b52f79d0f0c7c3e6535a506e3e5c62
MathijsA <61280363+MathijsA@users.noreply.github.com>
2021-06-01 15:44:22 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+
+<head>
+<title>Mathijs Arents</title>
+
+</head>
+<!-- portal-->
+<body>
+	login.php
+	<?php include("header.php"); ?>
+	<?php include("nav.php"); ?>
+
+<div class="content">
+  <div class="content_spacer"></div>
+</div>
+
+ <?php include("login2.php"); ?>
+<img src="../logo.PNG" alt="Flowers" style="width:auto;">
+</div>
+<div class="footer">
+  <?php include("../footer.php") ?>
+</div>
+
+</body>
